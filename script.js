ab4cf990278f772d87519e02db54e4451107c613
Aces <nicolas76.breant@otmail.fr>
2023-03-24 14:19:13 +0100

    grid cells + color picker + function to change color

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ console.log(grid);
 let lines = 16;
 let rows = lines;
 
-for (let i = 0; i < lines; ++i) {
-    let line = document.createElement("div");
-    line.classList.add("line");
-    for (let i = 0; i < rows; ++i) {
-        let row = document.createElement("div");
-        row.classList.add("cell");
-        line.appendChild(row);
-    }
-    grid.appendChild(line);
-}
-
 function changeWidth() {
     let x = document.querySelectorAll(".cell");
     console.log(x);
@@ -25,4 +14,47 @@ function changeWidth() {
     }
 }
 
-changeWidth();
+function createGrid() {
+    for (let i = 0; i < lines; ++i) {
+        let line = document.createElement("div");
+        line.classList.add("line");
+        for (let i = 0; i < rows; ++i) {
+            let row = document.createElement("div");
+            row.classList.add("cell");
+            line.appendChild(row);
+        }
+        grid.appendChild(line);
+    }
+
+    changeWidth();
+}
+
+
+createGrid();
+
+
+
+let mouseDown = false
+document.body.onmousedown = () => (mouseDown = true)
+document.body.onmouseup = () => (mouseDown = false)
+
+let color = "white";
+
+function changeColorPick(colorPicked) {
+    color = colorPicked;
+}
+
+function changeColorCell(e) {
+    if (e.type == 'mouseover' && !mouseDown) return
+    console.log(e);
+    console.log(color);
+    e.target.style.backgroundColor = color;
+}
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(item => {
+    item.addEventListener('mouseover', changeColorCell)
+})
+cells.forEach(item => {
+    item.addEventListener('mousedown', changeColorCell)
+})
